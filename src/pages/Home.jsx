e8dfe62334fd33ac28f48e95fc5d84adee724ec3
Rifannvl <riffanafendi@gmail.com>
2024-10-26 13:48:25 +0700

    menampilkan seluruh section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,16 @@
+import BaseLayout from "../layout/BaseLayout";
+import About from "./About";
+import Contact from "./Contact";
+import HeroSection from "./HeroSection";
+import Menu from "./Menu";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Home</h1>;
+  return (
+    <BaseLayout>
+      <HeroSection />
+      <Menu />
+      <About />
+      <Contact />
+    </BaseLayout>
+  );
 }
